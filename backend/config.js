c10022047f9cc63863b6b2f82dff47554dcdce7b
Master250 <tiamiyusewou@hotmail.com>
2021-02-22 20:33:20 +0100

    ...

---
+++ backend/config.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const app = {
+    host: process.env.APP_HOST,
+    port: process.env.APP_PORT
+}
+
+const db = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    url: process.env.DB_URL
+}
+
+console.log(app.db)
